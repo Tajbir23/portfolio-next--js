f470afd57b533918af225e1d5770de387eb13003
Tajbir islam <studenttajbirislam@gmail.com>
2024-06-22 21:41:22 +0600

    bg color

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" date-theme="black">
+    <html lang="en" >
       <body className={inter.className}>
         <Navbar />
         <TracingBeam>
