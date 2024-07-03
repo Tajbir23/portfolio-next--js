5e09abf09d7a9803197f0080b2188f77dbda8adf
Tajbir islam <studenttajbirislam@gmail.com>
2024-07-03 12:15:53 +0600

    add nodemailer

---
+++ src/app/email/route.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer"
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.USER_NAME,
+        pass: process.env.USER_PASSWORD,
+    }
+})
+export async function POST(request){
+    const body = await request.json()
+    console.log(body)
+
+    try {
+        const mail = await transporter.sendMail({
+            from: body.email,
+            to: process.env.USER_NAME,
+            subject: body.subject,
+            text: body.message
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+    return Response.json({
+        message: "Email send successful"
+    })
+}
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,35 @@
-
+'use client'
 
 const Contact = () => {
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    // Get form values
+    const subject = e.target.subject.value;
+    const email = e.target.email.value;
+    const message = e.target.message.value;
+    console.log(subject, email, message);
+
+    const res = await fetch("/email", {
+      method: "POST",
+      body: JSON.stringify({
+        subject: subject,
+        email: email,
+        message: message
+      })
+    });
+    const data = res.json()
+    console.log(data);
+    // Send form data to server or use fetch API
+    alert("Form submitted successfully!");
+  }
   return (
     <div id="contact" className="min-h-screen mx-10 bg-black text-white flex items-center justify-center">
-      <form className="bg-gray-900 p-8 rounded-lg w-full shadow-lg">
+      <form onSubmit={handleSubmit} className="bg-gray-900 p-8 rounded-lg w-full shadow-lg">
         <h2 className="text-2xl mb-6 font-bold">Contact Me</h2>
         <div className="flex gap-5 w-full flex-col md:flex-row">
         <div className="flex flex-col mb-4 ">
-          <label className="mb-2 text-gray-300" htmlFor="name">Name</label>
+          <label className="mb-2 text-gray-300" htmlFor="subject">Subject</label>
           <input
             className="p-3 bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             type="text"
