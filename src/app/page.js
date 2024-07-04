de958a1bcbdc40a8bc7dd1bb6bcfd53660d4fd62
Tajbir islam <studenttajbirislam@gmail.com>
2024-07-04 11:56:07 +0600

    add: education

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 
 import { CardHover } from "@/components/CardHover";
 import Contact from "@/components/Contact";
+import Education from "@/components/Education/Education";
 import Hero from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import Footer from "@/components/footer/Footer";
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <CardHover />
       <Projects />
+      <Education />
       <Contact />
       <Footer />
     </div>
---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,16 @@
+
+
+const Education = () => {
+  return (
+    <div className="mx-10">
+        <h1 className="text-4xl mb-10 font-bold text-white text-center">Education</h1>
+
+        <div className="bg-gray-900 rounded-md text-white flex flex-col m-auto p-5 justify-center">
+            <h1 className="text-2xl font-bold text-left mb-5">HSC</h1>
+            <p>Rajarhat karigori banijjik collage</p>
+        </div>
+    </div>
+  )
+}
+
+export default Education
