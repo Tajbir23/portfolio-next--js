c856c09f43be95f8d0bbb37e6e0bc2e9b76f2795
Tajbir islam <studenttajbirislam@gmail.com>
2024-07-04 13:46:43 +0600

    update: education

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -5,7 +5,7 @@ const Education = () => {
     <div className="mx-10">
         <h1 className="text-4xl mb-10 font-bold text-white text-center">Education</h1>
 
-        <div className="bg-gray-900 rounded-md text-white flex flex-col m-auto p-5 justify-center">
+        <div className="bg-gray-900 rounded-md text-white flex flex-col m-auto p-10 justify-center">
             <h1 className="text-2xl font-bold text-left mb-5">HSC</h1>
             <p>Rajarhat karigori banijjik collage</p>
         </div>
